8a8bb065150b08adf04b68cd354512f0696452f6
rajkanya-chaudhary <dc5068786@gmail.com>
2024-07-29 23:06:12 +0530

    this is a rest api create using Spring boot

---
+++ src/test/java/com/example/Apicreate/ApicreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apicreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
